d5a8df2afec922b01570a82e428fccacdb5b7fe1
konez2k <konez2k@gmail.com>
2017-12-01 23:55:51 +0100

    version and protocol bump

--- src/version.h
+++ src/version.h
@@ -30,11 +30,11 @@ static const int DATABASE_VERSION = 70508;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70020;
+static const int PROTOCOL_VERSION = 70030;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
-static const int MIN_PEER_PROTO_VERSION = 70020;
+static const int MIN_PEER_PROTO_VERSION = 70030;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
